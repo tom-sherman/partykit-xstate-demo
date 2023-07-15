f83d5f9e6d17631f3cfbe541e6f27c7934f86aeb
Tom Sherman <the.tomsherman@gmail.com>
2023-07-15 19:51:49 +0100

    Stable socket for dev mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
 import PartySocket from "partysocket";
 import { useParty } from "./socket";
-
-const socket = new PartySocket({
-  host: "localhost:1999",
-  room: "test",
-});
+import { useState } from "react";
 
 function App() {
+  // Get a stable reference to socket across saves in dev mode
+  // Kinda hacky lol, in the real world we could just define this at the module level
+  const [socket] = useState(
+    () =>
+      new PartySocket({
+        host: "localhost:1999",
+        room: "test",
+      })
+  );
   const { events } = useParty(socket);
 
   const nextEvents = (((events.at(-1) as any)?.nextEvents ?? []) as string[])
     // // Crudely filters out internal xstate events such as done.foo on nested states
     .filter((e) => !e.startsWith("done."));
-
   return (
     <>
       <form

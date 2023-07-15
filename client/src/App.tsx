447116e1a591f838926940d6979820966acf07f5
Tom Sherman <the.tomsherman@gmail.com>
2023-07-15 19:50:18 +0100

    Slightly better UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,12 +10,9 @@ const socket = new PartySocket({
 function App() {
   const { events } = useParty(socket);
 
-  const nextEvents = [
-    ...new Set([
-      "NEXT",
-      ...(((events.at(-1) as any)?.nextEvents as string[]) ?? []),
-    ]),
-  ];
+  const nextEvents = (((events.at(-1) as any)?.nextEvents ?? []) as string[])
+    // // Crudely filters out internal xstate events such as done.foo on nested states
+    .filter((e) => !e.startsWith("done."));
 
   return (
     <>
@@ -34,18 +31,23 @@ function App() {
           <legend>Event name</legend>
           {nextEvents.map((event) => (
             <div key={event}>
-              <input type="radio" name="event" value={event} /> {event}
+              <label>
+                <input type="radio" name="event" value={event} /> {event}
+              </label>
             </div>
           ))}
         </fieldset>
         <button type="submit">Send</button>
       </form>
       <ul>
-        {events.map((event, i) => (
-          <li key={i}>
-            <pre>{JSON.stringify(event)}</pre>
-          </li>
-        ))}
+        {events
+          .map((event, i) => (
+            <li key={i}>
+              {i === events.length - 1 && "Current: "}
+              <code>{JSON.stringify((event as any).state.value)}</code>
+            </li>
+          ))
+          .reverse()}
       </ul>
     </>
   );
